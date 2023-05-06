81585a86c1cf487006b61a2ac37864d4dd20b19a
Tinmix <469830014@qq.com>
2023-05-06 17:23:35 +0800

    fix bug

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import json
 import threading
 import pathlib
 import requests
+import re
 import yaml
 
 yaml.SafeDumper.org_represent_str = yaml.SafeDumper.represent_str
+
 
 def repr_str(dumper, data):
     if '\n' in data:
@@ -18,10 +20,7 @@ def repr_str(dumper, data):
 
 yaml.add_representer(str, repr_str)
 
-proxy = {
-    "http": "http://127.0.0.1:1080",
-    "https": "http://127.0.0.1:1080"
-}
+proxy = {"http": "http://127.0.0.1:1080", "https": "http://127.0.0.1:1080"}
 
 config = json.load(open("config/config.json", "r", encoding="utf-8"))
 header = {
@@ -37,6 +36,7 @@ rule_dict = {}
 task_list = []
 # https://dler.cloud/Rules/Clash/Provider/Reject.yaml
 # https://ghproxy.com/https://github.com/dler-io/Rules/blob/main/Clash/Provider/Reject.yaml
+
 
 def get_rule():
     while len(rule_queue) > 0:
@@ -62,13 +62,23 @@ def get_rule():
 
 ruleobj = yaml.load(res, Loader=yaml.FullLoader)
 
-for k, v in ruleobj["rule-providers"].items():
-    rule_queue.append({"name": k, "url": v["url"]})
+# https://github.com/dler-io/Rules/blob/main/Clash/Provider/Microsoft.yaml
+# https://dler.cloud/Rules/Clash/Provider/Reject.yaml
+# https://ghproxy.com/https://github.com/dler-io/Rules/blob/main/Clash/Provider/Reject.yaml
 
-for i in range(12):
-    t = threading.Thread(target=get_rule)
-    t.start()
-    task_list.append(t)
+for k, v in ruleobj["rule-providers"].items():
+    url: str = "https://ghproxy.com/https://raw.githubusercontent.com/dler-io/Rules/blob/main" + re.search(
+        f".*(/Clash/Provider/.+\.yaml)", v["url"]).group(1)
+    assert url.endswith(".yaml")
+    print(url)
+    assert "/Clash/Provider/" in url
+    ruleobj["rule-providers"][k]["url"] = url
+    rule_queue.append({"name": k, "url": url})
+
+# for i in range(12):
+#     t = threading.Thread(target=get_rule)
+#     t.start()
+#     task_list.append(t)
 
 groupobj: dict = yaml.load(open("config/group.yaml", "r", encoding="utf-8"),
                            Loader=yaml.FullLoader)
@@ -80,22 +90,23 @@ rule_files = rule_dir.glob("*.yaml")
 # print(groupobj["extra-rule-providers"].keys())
 assert "ChatGPT" in groupobj["extra-rule-providers"].keys()
 for item in rule_files:
-    if item.is_file() and item.stem not in groupobj["extra-rule-providers"].keys():
+    if item.is_file(
+    ) and item.stem not in groupobj["extra-rule-providers"].keys():
         assert item.stem != "ChatGPT"
         item.unlink()
 
-for groupname, item_group in groupobj["rule—groups"].items():
-    new_rulegroup = {"payload": []}
-    for item in item_group:
-        new_rulegroup["payload"] += rule_dict[item]
-    with open("rules/"+ groupname + ".yaml", "w", encoding="utf-8") as f:
-        yaml.dump(new_rulegroup, f)
-
+# for groupname, item_group in groupobj["rule—groups"].items():
+#     new_rulegroup = {"payload": []}
+#     for item in item_group:
+#         new_rulegroup["payload"] += rule_dict[item]
+#     with open("rules/" + groupname + ".yaml", "w", encoding="utf-8") as f:
+#         yaml.dump(new_rulegroup, f)
 
 examples = {
     "type": "http",
     "behavior": "classical",
-    "url": 'https://ghproxy.com/https://github.com/Blues-star/self_cla_rules/blob/main/rules/{}',
+    "url":
+    'https://ghproxy.com/https://github.com/Blues-star/self_cla_rules/blob/main/rules/{}',
     "path": "./Rules/{}",
     "interval": 86400,
 }
@@ -107,8 +118,9 @@ with open("script.py", "r", encoding="utf-8") as f:
     tree = ast.parse(code)
 
 ruleset_action = {}
-for groupname, item_group in groupobj["rule—groups"].items():
-    ruleset_action[groupname] = groupname
+for groupname, grouplist in groupobj["rule—groups"].items():
+    for item in grouplist:
+        ruleset_action[item] = groupname
 
 for node in ast.walk(tree):
     # 替换ruleset_action
@@ -116,27 +128,34 @@ for node in ast.walk(tree):
         if isinstance(node.targets[0], ast.Name):
             if node.targets[0].id == "ruleset_action":
                 node.value = ast.Dict(
-                    keys=[ast.Constant(value=k) for k in ruleset_action.keys()],
-                    values=[ast.Constant(value=v) for v in ruleset_action.values()],
+                    keys=[
+                        ast.Constant(value=k) for k in ruleset_action.keys()
+                    ],
+                    values=[
+                        ast.Constant(value=v) for v in ruleset_action.values()
+                    ],
                 )
-        
+
+rules = []
+for groupname, grouplist in groupobj["rule—groups"].items():
+    for item in grouplist:
+        rules.append(
+            f"RULE-SET,{item},{groupname}")
 
 new_rule_yaml = {
-    "rules":[f"RULE-SET,{i},{i}" for i in groupobj["rule—groups"].keys()] +["GEOIP,CN,国内流量","MATCH,Others"],  # noqa: E501
+    "rules": rules + ["GEOIP,CN,国内流量", "MATCH,Others"],  # noqa: E501
     "script": {
-        "code" : ast.unparse(tree)
+        "code": ast.unparse(tree)
     },
-    "rule-providers": groupobj["extra-rule-providers"]
+    "rule-providers": ruleobj["rule-providers"]
 }
-for groupname, item_group in groupobj["rule—groups"].items():
-    new_item = examples.copy()
-    new_item["url"] = examples["url"].format(groupname + ".yaml")
-    new_item["path"] = examples["path"].format(groupname)
-    new_rule_yaml["rule-providers"][groupname] = new_item
+for groupname, item_group in groupobj["extra-rule-providers"].items():
+    new_rule_yaml["rule-providers"]["groupname"] = item_group
 
 with open("new_rule.yaml", "w", encoding="utf-8") as f:
-        yaml.dump(new_rule_yaml,f,
-                            allow_unicode=True,
-                            default_flow_style=False,
-                            explicit_start=True,
-                            encoding="utf-8")
+    yaml.dump(new_rule_yaml,
+              f,
+              allow_unicode=True,
+              default_flow_style=False,
+              explicit_start=True,
+              encoding="utf-8")
